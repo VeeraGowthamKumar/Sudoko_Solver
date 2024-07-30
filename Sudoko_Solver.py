6a395db7282464867346bb69e3c986d0647e9a08
VeeraGowthamKumar <118497618+VeeraGowthamKumar@users.noreply.github.com>
2024-07-31 03:28:45 +0530

    Add files via upload

---
+++ Sudoko_Solver.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+
+def valid(board, num, pos):
+    row, col = pos
+    # Check row
+    if num in board[row]:
+        return False
+    # Check column
+    if num in [board[i][col] for i in range(9)]:
+        return False
+    # Check box
+    box_x, box_y = (col // 3) * 3, (row // 3) * 3
+    for i in range(box_y, box_y + 3):
+        for j in range(box_x, box_x + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for num in range(1, 10):
+        if valid(board, num, (row, col)):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0
+    return False
+
+# Example Sudoku Puzzle
+sudoku_puzzle = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,0,1,9,0,0,5],
+    [0,0,0,0,0,0,0,0,0]
+]
+
+print_board(sudoku_puzzle)
+if solve_sudoku(sudoku_puzzle):
+    print("\nSolution found:")
+else:
+    print("\nNo solution exists")
+print_board(sudoku_puzzle)
